8dcb8a2c999dc514d48e1211b22a46abb967a4bf
Edgar Plazola <edgarplazola@gmail.com>
2020-03-29 21:40:29 -0600

    http

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// import { init } from './js/usuarios-page'
+
+// import * as CRUD from './js/crud-provider';
+
+import {init} from './js/archivos-page'
+
+init();
+// CRUD.getUsuario(1).then(console.log);
+// CRUD.crearUsuario({
+//     name: 'Edgar',
+//     job: 'Ingeniero'
+// }).then(console.log);
+// CRUD.actualizarUsuario(1, {
+//     name: 'Juan',
+//     job: 'Zapatero'
+// }).then(console.log);
+
+// CRUD.borrarUsuario(1).then(console.log);
+
---
+++ src/js/http-provider.js
@@ -0,0 +1,63 @@
+const jokeUrl = 'https://api.chucknorris.io/jokes/random';
+const urlUsuarios = 'https://reqres.in/api/users?page=2';
+
+const cloudPreset = 'waeouejh';
+const cloudUrl = 'https://api.cloudinary.com/v1_1/dj4gcu6pq/upload';
+
+const obtenerChiste = async () => {
+
+    try {
+        const resp = await fetch(jokeUrl);
+
+        if (!resp.ok) {
+            throw 'No se pudo realizar la petición';
+        }
+
+        const { icon_url, id, value } = await resp.json();
+
+        return { icon_url, id, value };
+
+    } catch (err) {
+        throw err;
+    }
+
+
+
+}
+
+
+
+const obtenerUsuarios = async () => {
+    const resp = await fetch(urlUsuarios);
+    const { data: usuarios } = await resp.json();
+    return usuarios;
+}
+
+//Archivo subir :: File
+const subirImagen = async (archivoSubir) => {
+    const formData = new FormData();
+    formData.append('upload_preset', cloudPreset);
+    formData.append('file', archivoSubir);
+
+    try {
+        const resp = await fetch(cloudUrl, {
+            method: 'POST',
+            body: formData
+        });
+
+        if (resp.ok) {
+            const cloudResp = await resp.json();
+            return cloudResp.secure_url;
+        } else {
+            throw await resp.json();
+        }
+    } catch (err) {
+        throw err;
+    }
+}
+
+export {
+    obtenerChiste,
+    obtenerUsuarios,
+    subirImagen
+}
